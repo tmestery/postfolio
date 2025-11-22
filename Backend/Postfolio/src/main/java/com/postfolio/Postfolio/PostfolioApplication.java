b42e06c551a4a3db9ab360af0a03a86749de9f44
Tyler Mestery <tyler@mestery.com>
2025-11-22 17:26:23 -0600

    Utilized spring initializer to create spring boot backend skeleton.

---
+++ Backend/Postfolio/src/main/java/com/postfolio/Postfolio/PostfolioApplication.java
@@ -0,0 +1,13 @@
+package com.postfolio.Postfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostfolioApplication.class, args);
+	}
+
+}
